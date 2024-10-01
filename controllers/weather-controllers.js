820acfd0d17cee5485fb5fecd96e83f9f9e9ecc4
Ziad Meligy <89343979+ZiadMeligy@users.noreply.github.com>
2024-10-01 22:02:28 +0300

    weekly endpoint

--- controllers/weather-controllers.js
+++ controllers/weather-controllers.js
@@ -1,11 +1,14 @@
 // List of stuff to do 
-// 1. Embed Location to fetch weather data for a specific location
-// 2. Embed Time to fetch weather data for a next hour. next day, next week
-// 3. Embed Weather to fetch weather data for a specific weather condition (that depends on the API)
-// 4. Embed Users accounts and add responses to their history of weather data
-// 5. Allow only authenticated users to access the weather data
-// 6. stop the user from accessing the weather data if they have exceeded the limit of requests
+// 1. Embed Location to fetch weather data for a specific location  // DONE
 
+// 2. Embed Time to fetch weather data for a next hour. next day, weekly ->
+// 3. Embed Weather to fetch weather data for a specific weather condition (that depends on the API) for rain
+// 4. Embed Users accounts and add responses to their history of weather data -> 3 responses max
+// 5. Allow only authenticated users to access the weather data -> JWT token
+// 6. stop the user from accessing the weather data if they have exceeded the limit of requests -> payments method -> free trial 
+// 7. Embed the user's location to fetch the weather data for their location -> current-location-geo
+ 
+// import { getLocation } from "current-location-geo";
 
 export const getAllWeather = async (req, res) => {
     try {
@@ -93,3 +96,46 @@ export const getWeatherByHourToday = async (req, res) => {
     }
 }
 
+export const getWeatherWeekly = async (req, res) => {
+    try{
+        const location = req.params.location;
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=LJBU6P5FG3UY5WB2KUV65GSSG&contentType=json`);
+        if (!response.ok) {
+            return res.status(response.status).json({ error: 'Failed to fetch weather data' });
+        }
+        const data = await response.json();
+
+        const filteredData={
+            location: data.resolvedAddress,
+            weeklyWeather: data.days.slice(0,7).map(day => {
+                return {
+                    date: day.datetime,
+                    DayTemperature: day.tempmax,
+                    NightTemperature: day.tempmin,
+                    feelslike: day.feelslikemax,
+                    rainChance: day.percip,
+                    humidity: day.humidity,
+                    windSpeed: day.windspeedmax,
+                    description: day.conditions
+                }
+            })
+        } 
+        
+        res.json(filteredData);
+    }
+        catch(e){
+            console.log(e)
+            res.status(500).json({message: "Server Error"})
+        }
+    }
+
+
+// export const getMyLocationWeather = async (req, res) => {
+//     try{
+        
+    
+//     }
+//     catch(e){
+//         console.log(e)
+//         res.status(500).json({message: "Server Error"})
+//     }}
--- routes/weather routes.js
+++ routes/weather routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import {getAllWeather,getCurrentWeather,getWeatherByHourToday,getMyLocationWeather} from '../controllers/weather-controllers.js';
+import {getAllWeather,getCurrentWeather,getWeatherByHourToday,getWeatherWeekly} from '../controllers/weather-controllers.js';
 
 const weatherRouter = express.Router();
 
-weatherRouter.get('/', getMyLocationWeather);
+// weatherRouter.get('/', getMyLocationWeather);
 weatherRouter.get('/:location/all', getAllWeather);
 weatherRouter.get('/:location/current', getCurrentWeather);
 weatherRouter.get('/:location/hourly', getWeatherByHourToday);
+weatherRouter.get('/:location/weekly', getWeatherWeekly); 
 
 export default weatherRouter;
